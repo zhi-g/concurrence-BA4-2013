13dd38a2b65b6dcf794bba13214fe858111bd829
ZhivkaGucevska <zhivka.gucevska@epfl.ch>
2013-05-08 19:02:00 +0300

    Update kernel.c
    Un peu de code

--- Kernel Project/kernel.c
+++ Kernel Project/kernel.c
@@ -2,6 +2,34 @@
 #include "fpga/system_m.h"
 #include "kernel.h"
 
+#define MAX_PROCESS 10
+
+
+
+typedef struct Descripteur_{
+    struct Descripteur_ suivant;
+    Process prss;
+} Descripteur;
+
+Descripteur readyList[MAX_PROCESS];
+unsigned int nbProcess = 0;
+
+void creerProcessus(void (*f), int stackSize, int priority){
+    Process p = NULL;
+    int* stack = calloc(stackSize, sizeof(int));
+    if(stack != NULL){
+        p = newProcess((*f), stack, stackSize);
+    }
+    Descripteur d = { NULL, p };
+   if(nbProcess == 0){
+       readyList[nbProcess] = d;
+       ++nbProcess;
+   } else if (nbProcess >= MAX_PROCESS){
+       printf("Nombre de processus trop grand, aucun processus peut etre ajoute.");
+       exit (EXIT_FAILURE);
+   } 
+}
+
 
 int main() {
     return 0;
